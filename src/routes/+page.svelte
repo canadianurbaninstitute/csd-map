2deb2488464ed926fa4910b88b7b9adbf00a3320
Ananmay Sharan <32856566+ananmaysharan@users.noreply.github.com>
2024-06-03 14:21:20 -0400

    favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,119 +3,53 @@
 	import mapboxgl from "mapbox-gl";
 	import "../routes/styles.css";
 
-	import LegendItem from "../lib/ui/legends/LegendItem.svelte";
-	import Legend from "../lib/ui/legends/Legend.svelte";
-	import cds from "../routes/assets/mms-cds.geo.json";
-	import Select from "svelte-select";
-	import * as turf from "@turf/turf";
-
-	const cdnames = cds.features.map((feature) => feature.properties.CDNAME);
-
-	let cdSelected = "";
-
 	mapboxgl.accessToken =
 		"pk.eyJ1IjoiY2FuYWRpYW51cmJhbmluc3RpdHV0ZSIsImEiOiJjbG95bzJiMG4wNW5mMmlzMjkxOW5lM241In0.o8ZurilZ00tGHXFV-gLSag";
 
 	let map;
 
-	let geocoder;
-
-	const bounds = [
-		[-84.64951046, 40.17694922],
-		[-73.82038089, 47.7933883],
+	const layers = [
+		{
+			id: "1-10mil",
+			label: "1 to 10 million",
+			checked: true,
+			color: "#001d29",
+		},
+		{
+			id: "500k-1mil",
+			label: "500k to 1 million",
+			checked: true,
+			color: "#002332",
+		},
+		{
+			id: "100-500k",
+			label: "100k to 500k",
+			checked: true,
+			color: "#004663",
+		},
+		{
+			id: "75-100k",
+			label: "75k to 100k",
+			checked: true,
+			color: "#016895",
+		},
+		{ id: "50-75k", label: "50k to 75k", checked: true, color: "#018bc6" },
+		{ id: "40-50k", label: "40k to 50k", checked: true, color: "#01aef8" },
+		{ id: "30-40k", label: "30k to 40k", checked: true, color: "#34bef9" },
+		{ id: "20-30k", label: "20k to 30k", checked: true, color: "#67cefb" },
+		{ id: "10-20k", label: "10k to 20k", checked: true, color: "#99dffc" },
+		{ id: "1-10k", label: "1k to 10k", checked: true, color: "#cceffe" },
+		{ id: "0-1k", label: "0 to 1k", checked: true, color: "#ebf9ff" },
 	];
-
-	let ruralChecked = false;
-	let equityChecked = false;
-	let indigenousChecked = false;
-	let tier1Checked = true;
-	let mapLoaded = false; // Track the map's load state
-
-	// Function to update the map filter based on checkbox states
-	function updateMapFilter() {
-		if (!mapLoaded) return; // Ensure the map has loaded
-		const filters = ["all"];
-		if (ruralChecked) {
-			filters.push(["==", ["get", "Rural?"], "Yes"]);
-		}
-		if (equityChecked) {
-			filters.push(["==", ["get", "PRIORITY - Equity"], "YES"]);
-		}
-		if (indigenousChecked) {
-			filters.push(["==", ["get", "PRIORITY - Indigenous"], "YES"]);
-		}
-		// if (tier1Checked) {
-		// 	filters.push(["==", ["get", "Recommended - Tier 1"], "Yes"]);
-		// }
-		map.setFilter("mms-business", filters);
-
-		// Change the color of the points if tier1 is checked
-		if (tier1Checked) {
-			map.setPaintProperty("mms-business", "circle-color", [
-				"case",
-				["==", ["get", "Recommended - Tier 1"], "Yes"],
-				"#006501", // Color for Tier 1 points
-				"#5E32BD", // Default color for other points
-			]);
-		} else {
-			// Reset to default color when tier1 is not checked
-			map.setPaintProperty("mms-business", "circle-color", "#5E32BD"); // Default color for all points
-		}
-	}
-
-	function handleSelect(e) {
-		if (map.getLayer("highlighted-cd")) {
-			map.removeLayer("highlighted-cd");
-		}
-		if (map.getSource("highlighted-cd")) {
-			map.removeSource("highlighted-cd");
-		}
-
-		// reset cma selected variable
-		cdSelected = e.detail.value;
-
-		const filteredFeature = cds.features.find(
-			(feature) => feature.properties.CDNAME === cdSelected,
-		);
-
-		console.log(filteredFeature);
-
-		if (filteredFeature) {
-			const bbox = turf.bbox(filteredFeature);
-			map.fitBounds(bbox, { padding: 20 });
-		}
-
-		map.addSource("highlighted-cd", {
-			type: "geojson",
-			data: {
-				type: "FeatureCollection",
-				features: [filteredFeature],
-			},
-		});
-
-		map.addLayer({
-			id: "highlighted-cd",
-			type: "line",
-			source: "highlighted-cd",
-			paint: {
-				"line-color": "#333333",
-				"line-width": 2,
-			},
-		});
-
-		// not working with other filters yet
-
-		// map.setFilter('mms-business', ['==', ['get', 'Region/Municipality'], cdSelected]);
-	}
 
 	onMount(() => {
 		map = new mapboxgl.Map({
 			container: "map",
-			style: "mapbox://styles/canadianurbaninstitute/clwqlh0cl011j01qefx7o5ddn?fresh=true",
-			center: [-79.1, 44.07],
-			zoom: 5.8,
-			minZoom: 5.8,
-			maxZoom: 16,
+			style: "mapbox://styles/canadianurbaninstitute/clwv2xjui00hg01qp2w4t8slf?fresh=true",
+			center: [-89, 58],
+			zoom: 3.3,
+			maxZoom: 18,
+			minZoom: 2,
 			// maxBounds: bounds,
 			scrollZoom: true,
 			attributionControl: false,
@@ -128,84 +62,101 @@
 				customAttribution: "Canadian Urban Institute",
 			}),
 		);
-		// Geocoder Search
-
-
 
 		map.on("load", () => {
-			mapLoaded = true; // Set the map's load state to true when loaded
-
-			map.addSource("cds", {
-				type: "geojson",
-				data: cds,
-			});
-
-			map.setPaintProperty("mms-business", "circle-color", [
-				"case",
-				["==", ["get", "Recommended - Tier 1"], "Yes"],
-				"#006501", // Color for Tier 1 points
-				"#5E32BD", // Default color for other points
-			]);
-
-			// Add a new layer to visualize the polygon.
-			map.addLayer(
-				{
-					id: "cds",
-					type: "line",
-					source: "cds",
-					layout: {},
-					paint: {
-						"line-color": "#ddd",
-						"line-width": 1,
-					},
-				},
-				"mms-business",
-			);
-
-			map.addLayer({
-				id: "cds-fill",
-				type: "fill",
-				source: "cds", // reference the data source
-				layout: {},
-				paint: {
-					"fill-color": "#eee", // blue color fill
-					"fill-opacity": 0.1,
-				},
+			layers.forEach((layer) => {
+				const visibility = layer.checked ? "visible" : "none";
+				map.setLayoutProperty(layer.id, "visibility", visibility);
 			});
 
 			var popup = new mapboxgl.Popup({
-				closeButton: false,
-				closeOnClick: false,
+				closeButton: true,
+				closeOnClick: true,
 			});
 
-			map.on("mouseenter", "mms-business", function (e) {
-				// Change the cursor to a pointer when the mouse is over the business-mms layer
-				map.getCanvas().style.cursor = "pointer";
+			map.on(
+				"click",
+				[
+					"1-10mil",
+					"500k-1mil",
+					"100-500k",
+					"75-100k",
+					"50-75k",
+					"40-50k",
+					"30-40k",
+					"20-30k",
+					"10-20k",
+					"1-10k",
+					"0-1k",
+				],
+				function (e) {
+					// Change the cursor to a pointer when the mouse is over the business-mms layer
+					map.getCanvas().style.cursor = "pointer";
 
-				// Get the company name from the feature's properties
-				var company = e.features[0].properties.Company;
+					// Get the company name from the feature's properties
+					let name = e.features[0].properties.CSDNAME;
+					let pop = e.features[0].properties.pop;
 
-				// Set the popup content and location
-				popup
-					.setLngLat(e.lngLat)
-					.setHTML("<h4>" + company + "</h4>")
-					.addTo(map);
-			});
+					// Set the popup content and location
+					popup
+						.setLngLat(e.lngLat)
+						.setHTML("<h4>" + name + "</h4>" + "Population: " + pop)
+						.addTo(map);
+				},
+			);
 
-			map.on("mouseleave", "mms-business", function () {
-				// Reset the cursor when it leaves the business-mms layer
-				map.getCanvas().style.cursor = "";
+			map.on(
+				"mouseleave",
+				[
+					"1-10mil",
+					"500k-1mil",
+					"100-500k",
+					"75-100k",
+					"50-75k",
+					"40-50k",
+					"30-40k",
+					"20-30k",
+					"10-20k",
+					"1-10k",
+					"0-1k",
+				],
+				function () {
+					// Reset the cursor when it leaves the business-mms layer
+					map.getCanvas().style.cursor = "";
+				},
+			);
 
-				// Remove the popup
-				popup.remove();
-			});
+			map.on(
+				"mouseenter",
+				[
+					"1-10mil",
+					"500k-1mil",
+					"100-500k",
+					"75-100k",
+					"50-75k",
+					"40-50k",
+					"30-40k",
+					"20-30k",
+					"10-20k",
+					"1-10k",
+					"0-1k",
+				],
+				function () {
+					// Reset the cursor when it leaves the business-mms layer
+					map.getCanvas().style.cursor = "pointer";
+				},
+			);
 		});
 	});
 
-	$: ruralChecked, updateMapFilter();
-	$: equityChecked, updateMapFilter();
-	$: indigenousChecked, updateMapFilter();
-	$: tier1Checked, updateMapFilter();
+	function toggleLayer(layerId) {
+		const visibility = map.getLayoutProperty(layerId, "visibility");
+		if (visibility === "visible") {
+			map.setLayoutProperty(layerId, "visibility", "none");
+		} else {
+			map.setLayoutProperty(layerId, "visibility", "visible");
+		}
+	}
 </script>
 
 <svelte:head>
@@ -216,63 +167,28 @@
 	<script
 		src="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.min.js"
 	></script>
-	<link
-		rel="stylesheet"
-		href="https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-geocoder/v5.0.0/mapbox-gl-geocoder.css"
-		type="text/css"
-	/>
 </svelte:head>
 <div id="content">
 	<div id="controls">
-		<h4>My Main Street</h4>
-		<h5>Filter projects by:</h5>
-		<div>
-			<div class="checkbox-group">
-				<label>
-					<input type="checkbox" bind:checked={tier1Checked} />
-					Tier 1 - Recommended
-				</label>
-				<label>
-					<input type="checkbox" bind:checked={ruralChecked} />
-					Rural
-				</label>
-				<label>
-					<input type="checkbox" bind:checked={equityChecked} />
-					Equity-deserving
-				</label>
-				<label>
-					<input type="checkbox" bind:checked={indigenousChecked} />
-					Indigenous
-				</label>
-			</div>
-		</div>
-		<h5>Select Region</h5>
-		<div class="select-container">
-			<Select
-				id="select"
-				items={cdnames}
-				value={cdSelected}
-				on:input={handleSelect}
-				--background="white"
-				--item-color="black"
-				--item-is-active-color="black"
-				--item-is-active-bg="#eee"
-			/>
-		</div>
-		<div class="legend">
-			<h4>Legend</h4>
-			<LegendItem
-				variant={"circle"}
-				label={"Tier 1 - Recommended"}
-				bgcolor={"#006501"}
-				bordercolor={"#ffffff"}
-			/>
-			<LegendItem
-				variant={"circle"}
-				label={"Business Sustainability"}
-				bgcolor={"#5E32BD"}
-				bordercolor={"#fff"}
-			/>
+		<h4>Census Subdivision Map</h4>
+		<h5>Click on a CSD to view information about it.</h5>
+		<h5>Population:</h5>
+		<div class="controls">
+			{#each layers as layer}
+				<div class="label">
+					<input
+						type="checkbox"
+						id={layer.id}
+						on:change={() => toggleLayer(layer.id)}
+						checked
+					/>
+					<div
+						class="box"
+						style="display: block;width:20px; height: 20px; background-color: {layer.color}; border: 1px solid #00243350; border-radius: 0.2em"
+					></div>
+					<label for={layer.id}>{layer.label}</label>
+				</div>
+			{/each}
 		</div>
 	</div>
 	<div id="map" />
@@ -297,18 +213,7 @@
 		padding: 1em;
 		display: flex;
 		flex-direction: column;
-		gap: 1em;
-	}
-
-	.checkbox-group {
-		display: flex;
-		flex-direction: column;
-		border: 1px solid #eee;
-		padding: 1em;
-	}
-
-	.select-container {
-		margin-bottom: 1em;
+		gap: 2em;
 	}
 
 	#map {
@@ -318,12 +223,10 @@
 		border-left: 1px solid #eee;
 	}
 
-	.legend {
-		position: relative;
-		background-color: #fff;
-		padding: 1em;
-		border-radius: 0.6em;
-		border: 1px solid #eee;
-		margin: 0 0 0.5em 0;
+	.label {
+		display: flex;
+		flex-direction: row;
+		padding: 0.5em;
+		gap: 0.5em;
 	}
 </style>
